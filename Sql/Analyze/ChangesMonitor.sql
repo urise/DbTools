fa59cb3ce276b1798803c283671f9e224b241f8e
Yuriy Dolzhikov <urise@i.ua>
2013-07-26 17:26:00 +0300

    ChangesMonitor

--- Sql/Analyze/ChangesMonitor.sql
+++ Sql/Analyze/ChangesMonitor.sql
@@ -6,9 +6,37 @@ create table __oplogs (
 	tablename varchar(128),
 	pk int null
 )
--- creating triggers for all the tables
-SELECT  
-	'create trigger tr_oplogs_' + tc.TABLE_NAME + ' on dbo.' + tc.TABLE_NAME + ' ' + 
+alter view v_oplogs_pk_for_monitoring as
+select table_name, column_name
+from 
+	(select ccu.table_name, column_name, c.system_type_id, 
+		count(*) over (partition by ccu.table_name, ccu.constraint_name) as column_count
+	 from information_schema.table_constraints tc
+		inner join information_schema.constraint_column_usage ccu
+			on tc.constraint_name = ccu.constraint_name
+			  and tc.table_name = ccu.table_name
+		inner join sys.all_columns c
+			on ccu.column_name = c.name and ccu.table_name = object_name(object_id)
+	where tc.constraint_type = 'Primary Key' ) Q
+where Q.column_count = 1 and Q.system_type_id = 56
+go
+alter view v_oplogs_tables_for_monitoring as
+select t.name as table_name, 
+	IsNull(pk.column_name, c.name) as column_name
+from sys.tables t
+  left join v_oplogs_pk_for_monitoring pk
+    on t.name = pk.table_name
+  left join sys.all_columns c
+    on t.name = object_name(c.object_id) and c.system_type_id = 56
+		and (c.name = t.name + 'Id' 
+			or substring(t.name, 1, 3) = 'tbl'
+			  and c.name = substring(t.name, 4, len(t.name) - 3) + 'Id'
+		  )
+where 
+	pk.column_name is not null or c.name is not null		  
+go
+select
+	'create trigger tr_oplogs_' + table_name + ' on dbo.' + table_name + ' ' + 
 	N'after insert, delete, update
 	as
 	declare @operation varchar(1), @id int
@@ -19,18 +47,9 @@ SELECT
 	else
 		select @operation = ''D'', @id = ' + column_name + N' from deleted
 	insert into __oplogs (dt, operation, tablename, pk)
-	values (GETDATE(), @operation, ''' + tc.TABLE_NAME + ''', @id)'
-FROM    information_schema.table_constraints tc
-            JOIN information_schema.constraint_column_usage ccu
-                ON tc.constraint_name = ccu.Constraint_name
-WHERE   tc.constraint_type = 'Primary Key'
-
-
--- drop triggers
-select 'drop trigger tr_oplogs_' + tc.TABLE_NAME
-FROM    information_schema.table_constraints tc
-            JOIN information_schema.constraint_column_usage ccu
-                ON tc.constraint_name = ccu.Constraint_name
-            join sys.all_columns c on ccu.column_name = c.name
-              and tc.TABLE_NAME = OBJECT_NAME(c.object_id)
-WHERE   tc.constraint_type = 'Primary Key'
+	values (GETDATE(), @operation, ''' + table_name + ''', @id)'
+from v_oplogs_tables_for_monitoring
+       
+select 'drop trigger ' + name
+from sys.triggers
+where name like 'tr_oplogs_%'
